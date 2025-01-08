317674a22092bc1a04b696c026a2b8471e49f13d
Joel Salazar <joel.salazar@itoc.com.au>
2025-01-08 15:28:15 +1100

    Added DynamoDB table and IAM Role for lambda using CDK

--- cdk/infra/infra_stack.py
+++ cdk/infra/infra_stack.py
@@ -2,8 +2,10 @@ import os
 from pathlib import Path
 from typing import Any
 
-from aws_cdk import Stack
+from aws_cdk import RemovalPolicy, Stack
 from aws_cdk import aws_apigateway as apigateway
+from aws_cdk import aws_dynamodb as dynamodb
+from aws_cdk import aws_iam as iam
 from aws_cdk import aws_lambda as lambda_
 from constructs import Construct
 from dotenv import load_dotenv
@@ -14,6 +16,38 @@ load_dotenv()
 class TaskTrackerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # Create the DynamoDB table
+        table = dynamodb.Table(
+            self,
+            id="TaskTrackerTable",
+            table_name="task_tracker",
+            partition_key=dynamodb.Attribute(
+                name="id",
+                type=dynamodb.AttributeType.STRING,
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            encryption=dynamodb.TableEncryption.AWS_MANAGED,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # Create the IAM role for Lambda
+        lambda_role = iam.Role(
+            self,
+            "TaskTrackerFunctionExecutionRole",
+            role_name="TaskTrackerFunctionExecutionRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"),
+            ],
+        )
+
+        lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=["dynamodb:*"],
+                resources=[table.table_arn],
+            )
+        )
 
         # Lambda Function for FastAPI entrypoint handler
         # asset logic is required to handle both unit tests and cdk synth
@@ -27,7 +61,10 @@ class TaskTrackerStack(Stack):
             code=lambda_.Code.from_asset(str(asset)),
             function_name="TaskTrackerAPI",
             handler="main.handler",
+            role=lambda_role,
             environment={
+                "aws_region": os.environ.get("AWS_REGION", "ap-southeast-2"),
+                "db_table": table.table_name,
                 "environment": os.environ.get("ENVIRONMENT", "DEV"),
                 "log_level": os.environ.get("LOG_LEVEL", "INFO"),
             },
--- cdk/tests/unit/test_infra_stack.py
+++ cdk/tests/unit/test_infra_stack.py
@@ -9,9 +9,35 @@ def test_lambda_api_created() -> None:
     template = assertions.Template.from_stack(stack)
 
     template.has_resource_properties(
+        "AWS::DynamoDB::Table",
+        {
+            "KeySchema": [{"AttributeName": "id", "KeyType": "HASH"}],
+            "TableName": "task_tracker",
+            "BillingMode": "PAY_PER_REQUEST",
+            "SSESpecification": {"SSEEnabled": True},
+        },
+    )
+
+    template.has_resource_properties(
+        "AWS::IAM::Role",
+        {
+            "RoleName": "TaskTrackerFunctionExecutionRole",
+            "AssumeRolePolicyDocument": {
+                "Statement": [
+                    {
+                        "Action": "sts:AssumeRole",
+                        "Effect": "Allow",
+                        "Principal": {"Service": "lambda.amazonaws.com"},
+                    }
+                ]
+            },
+        },
+    )
+
+    template.has_resource_properties(
         "AWS::Lambda::Function",
         {
-            "Environment": {"Variables": {}},
+            "Environment": {"Variables": {"aws_region": "ap-southeast-2", "db_table": {}}},
             "Handler": "main.handler",
             "FunctionName": "TaskTrackerAPI",
             "Runtime": "python3.13",
